a664c29e14fa0a334af861b5421e7a8dc42989bd
fatemehesmaeli <asfaty200@gmail.com>
2024-08-12 02:57:27 -0700

    viewset

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns =[
     path('',views.index),
+    path('menu/',views.MenuView.as_view()),
+    path('menu/<int:pk>',views.SingleMenuView.as_view())
 ]
