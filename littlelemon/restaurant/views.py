88e8ffd70205d9a642b05d53d73dda86f2e0f027
fatemehesmaeli <asfaty200@gmail.com>
2024-08-12 06:06:37 -0700

    permission

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest,HttpResponse
 from .models import Booking,Menu
 from .serializers import MenuSerializer,BookingSerializer
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser,IsAuthenticated
 from rest_framework.decorators import parser_classes
+
 # Create your views here.
 
 # def home(request):
@@ -16,15 +17,28 @@ def index(request):
 class MenuView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class=MenuSerializer
-    permission_classes=[IsAuthenticated]
+    def get_permissions(self):
+        permission_classes=[]
+        if self.request.method != 'GET':
+
+            permission_classes=[IsAdminUser]
+        return [permission() for permission in permission_classes]
+    
+        
 
 
 class SingleMenuView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
-    permission_classes = [IsAuthenticated]
+    def get_permissions(self):
+        permission_classes=[]
+        if self.request.method != 'GET':
+
+            permission_classes=[IsAdminUser]
+        return [permission() for permission in permission_classes]
 
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
